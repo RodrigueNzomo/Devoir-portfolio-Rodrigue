e5f51f0323362a42a985c833af6031d2def2fb1f
Rodrigue Nzomo Nzogang <nzomo.rodrigue@tmrcomputing.com>
2024-08-10 10:57:20 +0100

    Mon site mise à jour Portofolio Rodrigue

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,4 @@
-/*!
-* Start Bootstrap - Freelancer v7.0.7 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+
 
 window.addEventListener('DOMContentLoaded', event => {
 
@@ -20,7 +13,7 @@ window.addEventListener('DOMContentLoaded', event => {
         } else {
             navbarCollapsible.classList.add('navbar-shrink')
         }
-
+           
     };
 
     // Shrink the navbar 
